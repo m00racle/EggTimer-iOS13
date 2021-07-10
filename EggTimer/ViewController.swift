9ddb2b5c98b1478460aa50d877eb5e0117662869
m00racle <yanuar.prakosa@gmail.com>
2021-07-10 09:47:21 +0700

    using dictionary instead of switch for eggTime

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -12,24 +12,19 @@ class ViewController: UIViewController {
     let softTime = 5
     let mediumTime = 8
     let hardTime = 12
+    
+//    make dictionary on eggTime:
+    let eggTimes = ["Soft":5, "Medium":8, "Hard":12]
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
         let buttonTitle = sender.titleLabel?.text ?? "nil"
         let msgTitle = "button title: "
         print(msgTitle + buttonTitle )
         
-        //var boilTime:Int
-        
-        // let's make the switch flow control
-        switch buttonTitle {
-        case "Soft":
-            print(softTime)
-        case "Medium":
-            print(mediumTime)
-        case "Hard":
-            print(hardTime)
-        default:
-            print("Unidentified hardness")
+//        make the dictionary to put the value for title label
+        let eggTime = eggTimes[buttonTitle]
+        if (eggTime != nil) {
+            print(eggTime!)
         }
     }
     
